7bead297cc4f8244a917aa07adff79e860fc372c
lhdjply <lhdjply@126.com>
2025-02-18 10:27:07 +0800

    feat: update babl to 0.1.110-1
    Signed-off-by: lhdjply <lhdjply@126.com>

--- git-version.h
+++ git-version.h
@@ -1,6 +1,6 @@
 #ifndef __BABL_GIT_VERSION_H__
 #define __BABL_GIT_VERSION_H__
 
-#define BABL_GIT_VERSION "BABL_0_1_106-17-gd3337db"
+#define BABL_GIT_VERSION "BABL_0_1_108-22-gef6409d"
 
 #endif /* __BABL_GIT_VERSION_H__ */
